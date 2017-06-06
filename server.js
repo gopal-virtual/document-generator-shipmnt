1ba590a77d7a9586efcf4a4a181da43e0fc575bf
= <=>
2017-06-06 17:00:48 +0530

    log added on server start

--- server.js
+++ server.js
@@ -163,4 +163,11 @@ function createDoc(req, res) {
     })
 }
 
-app.listen(9000);
+var server = app.listen(9000, 'localhost', function() {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+
+})
